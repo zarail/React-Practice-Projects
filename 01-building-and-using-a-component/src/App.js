0329ceefc503ae2e021777654d87450fb9f36f56
Zahra Ilkhan <zahraa1991@gmail.com>
2023-11-06 16:18:55 +0100

    first commit

---
+++ 01-building-and-using-a-component/src/App.js
@@ -0,0 +1,20 @@
+export function MainGoal() {
+  return (
+    <header>
+      <h1>My main Goal:</h1>
+      <p>
+        Learn React.js in depth and from the ground up, in order to start my career in tech. 😊
+      </p>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <div id="app">
+      <MainGoal />
+    </div>
+  );
+}
+
+export default App;
