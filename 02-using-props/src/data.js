29be34c006e973f509a8bf8d71fa5b82f0065977
Zara <122231647+zarail@users.noreply.github.com>
2023-11-08 14:33:00 +0100

    split css component stylings

--- 02-using-props/src/data.js
+++ 02-using-props/src/data.js
@@ -1,33 +1,33 @@
-import aus from './assets/au-flag.png';
-import ger from './assets/gm-flag.png';
-import lie from './assets/ls-flag.png';
-import lux from './assets/lu-flag.png';
-import swi from './assets/sz-flag.png';
+import aus from "./assets/au-flag.png";
+import ger from "./assets/gm-flag.png";
+import lie from "./assets/ls-flag.png";
+import lux from "./assets/lu-flag.png";
+import swi from "./assets/sz-flag.png";
 
 export const COUNTRIES = [
   {
     flag: aus,
-    name: 'Austria',
-    capital: 'Vienna'
+    name: "Austria",
+    capital: "Vienna",
   },
   {
     flag: ger,
-    name: 'Germany',
-    capital: 'Berlin'
+    name: "Germany",
+    capital: "Berlin",
   },
   {
     flag: lie,
-    name: 'Liechtenstein',
-    capital: 'Vaduz'
+    name: "Liechtenstein",
+    capital: "Vaduz",
   },
   {
     flag: lux,
-    name: 'Luxembourg',
-    capital: 'Luxembourg'
+    name: "Luxembourg",
+    capital: "Luxembourg",
   },
   {
     flag: swi,
-    name: 'Switzerland',
-    capital: 'Bern'
-  }
+    name: "Switzerland",
+    capital: "Bern",
+  },
 ];
