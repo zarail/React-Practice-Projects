6f6009b325a97fe6a23cb6f37b79cbeb7f00faf6
Zara <122231647+zarail@users.noreply.github.com>
2023-11-08 13:45:59 +0100

    props project done

--- 02-using-props/src/App.jsx
+++ 02-using-props/src/App.jsx
@@ -1,20 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { COUNTRIES } from "./data.js";
+
+function Header() {
+  return (
+    <header>
+      <h1>German is the official language in these countries:</h1>
+    </header>
+  );
+}
+
+function Country({ flag, name, capital }) {
+  return (
+    <li>
+      <img src={flag} alt={name} />
+      <h2>{name}</h2>
+      <p>Capital: {capital}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <section id="countries">
+          <ul>
+            <Country
+              flag={COUNTRIES[0].flag}
+              name={COUNTRIES[0].name}
+              capital={COUNTRIES[0].capital}
+            />
+            <Country {...COUNTRIES[1]} />
+            <Country {...COUNTRIES[2]} />
+            <Country {...COUNTRIES[3]} />
+            <Country {...COUNTRIES[4]} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ 02-using-props/src/data.js
@@ -0,0 +1,33 @@
+import aus from './assets/au-flag.png';
+import ger from './assets/gm-flag.png';
+import lie from './assets/ls-flag.png';
+import lux from './assets/lu-flag.png';
+import swi from './assets/sz-flag.png';
+
+export const COUNTRIES = [
+  {
+    flag: aus,
+    name: 'Austria',
+    capital: 'Vienna'
+  },
+  {
+    flag: ger,
+    name: 'Germany',
+    capital: 'Berlin'
+  },
+  {
+    flag: lie,
+    name: 'Liechtenstein',
+    capital: 'Vaduz'
+  },
+  {
+    flag: lux,
+    name: 'Luxembourg',
+    capital: 'Luxembourg'
+  },
+  {
+    flag: swi,
+    name: 'Switzerland',
+    capital: 'Bern'
+  }
+];
