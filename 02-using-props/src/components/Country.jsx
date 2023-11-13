d5b4c82ff4e8919ad4ed7cc91bf5ed320dd39a8a
Zara <122231647+zarail@users.noreply.github.com>
2023-11-13 09:48:32 +0100

    comment on props (country component)

--- 02-using-props/src/components/Country.jsx
+++ 02-using-props/src/components/Country.jsx
@@ -7,3 +7,29 @@ export default function Country({ flag, name, capital }) {
     </li>
   );
 }
+
+/*
+in the function parameters part, instead of "props" we can use "object destructuring" to get the props, so we can use the props directly in the function:
+
+function Country(props) { // props is an object with the props
+  return (
+    <li>
+      <img src={props.flag} alt={props.name} />
+      <h2>{props.name}</h2>
+      <p>Capital: {props.capital}</p>
+    </li>
+  );
+}
+
+has been changed to:
+
+function Country({ flag, name, capital }) { // props is an object with the props
+  return (
+    <li>
+      <img src={flag} alt={name} />
+      <h2>{name}</h2>
+      <p>Capital: {capital}</p>
+    </li>
+  );
+}
+*/
