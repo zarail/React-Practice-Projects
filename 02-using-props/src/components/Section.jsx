ecc7adae54669c74e6bffe867b14590f35d9f9d5
Zara <122231647+zarail@users.noreply.github.com>
2023-11-14 14:56:55 +0100

    comment on section component

--- 02-using-props/src/components/Section.jsx
+++ 02-using-props/src/components/Section.jsx
@@ -1,4 +1,5 @@
-// a wrapper component, with props: title, children
+// we add section component bcs the app will probably have more than one section with an <h2> and other children in the future, so we want to make it reusable
+// Section is a wrapper component, with props: title, children
 // these ...props are called forwarded props or proxy props
 
 export default function Section({ title, children, ...props }) {
