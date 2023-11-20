4540a88383965341eca57e11e9145e40c6434e18
Zahra Ilkhan <zahraa1991@gmail.com>
2023-11-20 19:41:33 +0100

    comment the function return

--- 05-creating-flexible-components/src/Button.jsx
+++ 05-creating-flexible-components/src/Button.jsx
@@ -1,4 +1,6 @@
 export default function Button({ children, mode = 'filled', Icon, ...props}) {
+  // "mode = 'filled'" as a default
+
   let cssClasses = `button ${mode}-button`; // string interpolation
 
   if (Icon) {
@@ -21,6 +23,28 @@ export default function Button({ children, mode = 'filled', Icon, ...props}) {
   );
 }
 
-// we pass "mode = 'filled'" as a default value to the Button component.
+// {Icon && (...)}: If Icon is truthy (meaning it exists), then the content inside the parentheses is rendered.
 
-// we wrap the icon with a <span className="button-icon"> to achieve the target look
+// <span className="button-icon"><Icon /></span>: This code is rendering an <Icon /> component inside a <span> with the class name "button-icon." This is likely a pattern for rendering an icon within the button.
+
+// <span>{children}</span>: This code is rendering the children prop inside a <span>. This is likely a pattern for rendering the text inside the button.
+
+/*
+<span> in HTML:
+
+In HTML, the <span> element is an inline container used to group and apply **styles** to inline elements. It doesn't add any specific styling or layout to the content within it; it's more of a generic container. The <span> element is often used when you want to apply CSS styles or manipulate the content using JavaScript without introducing any semantic meaning.
+
+Here's a basic example of how the <span> element can be used in HTML:
+
+<p>This is a <span style="color: blue; font-weight: bold;">blue and bold</span> text.</p>
+
+In this example, the <span> element is used to group the text "blue and bold" so that a specific style can be applied to it.
+
+In this app:
+
+<span className="button-icon">
+  <Icon />
+</span>
+
+Here, the <span> element is being used to contain an <Icon /> component. It's likely used to apply specific styling or to group the icon within the structure of the component. The class name "button-icon" suggests that it might be used for styling purposes related to a button or an icon in a button.
+*/
