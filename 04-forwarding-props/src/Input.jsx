bda467c436de6113ce05118dd7590a5684206a02
Zahra Ilkhan <zahraa1991@gmail.com>
2023-11-20 10:30:21 +0100

    change condition to ternery

--- 04-forwarding-props/src/Input.jsx
+++ 04-forwarding-props/src/Input.jsx
@@ -1,16 +1,39 @@
-// here we need to modify the Input component to conditionally render either a <textarea> or an <input> based on the value of the richText prop.
-// we should also forward all other props to the rendered element.
+// here we conditionally render either a <textarea> or an <input> based on the value of the richText prop.
+// we also forward all other props to the rendered element. (...props)
 
 export default function Input({ richText, ...props }) {
-  if (richText) {
+  return richText ? <textarea {...props} /> : <input {...props} />;
+}
+
+/* OR:
+if (richText) {
     return <textarea {...props} />;
   } else {
     return <input {...props} />;
   }
-}
+*/
 
-// OR:
-// return richText ? <textarea {...props} /> : <input {...props} />;
+/*
+1. Function Signature:
+
+export default function Input({ richText, ...props })
+
+--> This is a functional component named Input. It takes an object as its argument, and it uses object destructuring to extract the richText property and collect the rest of the properties in the props object.
+
+2. Conditional Rendering:
+
+if (richText) { return <textarea {...props} />; } else { return <input {..props} />; }
+
+--> This is a conditional statement. If richText is truthy, it returns a <textarea> element with all the props passed to it using the spread operator ({...props}). If richText is falsy, it returns an <input> element with the same set of props.
+
+Explanation:
+
+- If the richText prop is true, the Input component will render a <textarea> with all the additional props passed to it.
+
+- If the richText prop is false or not provided, the Input component will render an <input> with the same set of additional props.
+
+In summary, this code defines a React functional component that conditionally renders either a <textarea> or an <input> based on the value of the richText prop. It uses object destructuring to extract the richText prop and the rest of the props, and it uses the spread operator to pass those props to the rendered element.
+*/
 
 /*
 In this code:
