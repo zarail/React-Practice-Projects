7ba351091a00c7e0fb891e5eb1efcec0095a4783
Zara <122231647+zarail@users.noreply.github.com>
2023-11-21 10:50:57 +0100

    add game container div and the 1st hard-coded data

--- 06-tic-tac-toe/src/App.jsx
+++ 06-tic-tac-toe/src/App.jsx
@@ -1,5 +1,33 @@
 function App() {
-  return <p>Coming soon ...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      GAME LOG
+    </main>
+  );
 }
 
 export default App;
+
+/*
+<main></main> is the the warpper of the main content of the page (the different building blocks)
+
+for the styling purposes:
+- inside the <div> to game container there are the players aand the game board
+- outside the <div> there is the game log
+
+we add <span> for the styling purposes
+
+*/
