6ec0de9499839d87bb66dbd5cb65de74dc1b6d89
Zahra Ilkhan <zahraa1991@gmail.com>
2023-11-27 13:17:29 +0100

    import winning combinations

--- 06-tic-tac-toe/src/App.jsx
+++ 06-tic-tac-toe/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 
 // helper function to get rid of the repetition of the code:
 function deriveActivePlayer(gameTurns) {
