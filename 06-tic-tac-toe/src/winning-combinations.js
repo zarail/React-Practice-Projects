b007cef695489ae15a17fce2badb4782a9532e4a
Zahra Ilkhan <zahraa1991@gmail.com>
2023-11-27 13:16:13 +0100

    add winning combinations

---
+++ 06-tic-tac-toe/src/winning-combinations.js
@@ -0,0 +1,46 @@
+export const WINNING_COMBINATIONS = [
+  [
+    { row: 0, column: 0 },
+    { row: 0, column: 1 },
+    { row: 0, column: 2 },
+  ],
+  [
+    { row: 1, column: 0 },
+    { row: 1, column: 1 },
+    { row: 1, column: 2 },
+  ],
+  [
+    { row: 2, column: 0 },
+    { row: 2, column: 1 },
+    { row: 2, column: 2 },
+  ],
+  [
+    { row: 0, column: 0 },
+    { row: 1, column: 0 },
+    { row: 2, column: 0 },
+  ],
+  [
+    { row: 0, column: 1 },
+    { row: 1, column: 1 },
+    { row: 2, column: 1 },
+  ],
+  [
+    { row: 0, column: 2 },
+    { row: 1, column: 2 },
+    { row: 2, column: 2 },
+  ],
+  [
+    { row: 0, column: 0 },
+    { row: 1, column: 1 },
+    { row: 2, column: 2 },
+  ],
+  [
+    { row: 0, column: 2 },
+    { row: 1, column: 1 },
+    { row: 2, column: 0 },
+  ],
+];
+
+// The last comma in the arrays of arrays/objects is called a "trailing comma".
+// It is part of the ECMAScript 5 specification.
+// Trailing commas are optional, but they can make it easier to add or remove elements from an array or object without having to modify the previous line of code.
