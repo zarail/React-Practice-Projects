3b6d44bb90eed05329fab9ac5f4a57e6ea30db5c
Zahra Ilkhan <zahraa1991@gmail.com>
2023-11-27 15:42:31 +0100

    make code lean and more readable

--- 06-tic-tac-toe/src/App.jsx
+++ 06-tic-tac-toe/src/App.jsx
@@ -5,7 +5,14 @@ import Log from "./components/Log";
 import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
-const initialGameBoard = [
+// the uppercase constants are general constants (they are not specific to a component)
+
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2",
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
@@ -22,20 +29,7 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({
-    X: "Player 1",
-    O: "Player 2",
-  });
-
-  const [gameTurns, setGameTurns] = useState([]); // the state which controls the whole game!!!
-  // if you want to reset the game, you can just set the gameTurns to an empty array!
-
-  // const [hasWinner, setHasWinner] = useState(false); // we don't need this approach because we have the winning combinations, so it's redundant!
-  // const [activePlayer, setActivePlayer] = useState("X");
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
+function deriveGameBoard(gameTurns) {
   let gameBoard = [...initialGameBoard.map(array => [...array])]; // a deep copy of the initialGameBoard array (outer and inner arrays) --> a new array in the memory after every rematching!
 
   for (const turn of gameTurns) {
@@ -45,6 +39,10 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
   let winner; // it's undefined by default (OR: let winner = null;)
 
   // the following function will be executed every time the app component is rendered (or the gameTurns state is updated):
@@ -62,6 +60,21 @@ function App() {
     }
   }
 
+  return winner; // either the name of winner or undefined
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const [gameTurns, setGameTurns] = useState([]); // the state which controls the whole game!!!
+  // if you want to reset the game, you can just set the gameTurns to an empty array!
+
+  // const [hasWinner, setHasWinner] = useState(false); // we don't need this approach because we have the winning combinations, so it's redundant!
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -97,13 +110,13 @@ function App() {
         <ol id="players" className="highlight-player">
           {/* here we output the "Player" component: */}
           <Player
-          initialName="Player 1"
+          initialName={PLAYERS.X}
           symbol="X"
           isActive={activePlayer === "X"}
           onChangeName={handlePlayerNameChange}
           />
           <Player
-          initialName="Player 2"
+          initialName={PLAYERS.O}
           symbol="O"
           isActive={activePlayer === "O"}
           onChangeName={handlePlayerNameChange}
