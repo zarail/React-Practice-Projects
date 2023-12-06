1153d60e7425863e91dc2a5e6f37a402ec0fafed
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-06 17:33:30 +0100

    add inline styling

--- 09-styling/src/components/Header.jsx
+++ 09-styling/src/components/Header.jsx
@@ -6,7 +6,13 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p // this is how inline styles are added in React:
+        style={{
+          fontStyle: 'italic',
+          fontSize: '1rem',
+          color: 'var(--color-primary)',
+        }}
+      >A community of artists and art-lovers.</p>
     </header>
   );
 }
