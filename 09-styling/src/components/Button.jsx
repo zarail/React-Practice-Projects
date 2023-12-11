a9cc5bc3b4f3d1ca271ad49f80bffb509786a65e
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-11 13:59:31 +0100

    refactor Button component styling with Tailwind CSS

--- 09-styling/src/components/Button.jsx
+++ 09-styling/src/components/Button.jsx
@@ -1,3 +1,11 @@
+export default function Button( { children, ...props }) {
+  return <button
+    className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500" {...props}>
+    {children}
+  </button>
+  }
+
+/*
 import { styled } from "styled-components";
 
 const Button = styled.button`
@@ -16,3 +24,4 @@ const Button = styled.button`
 `;
 
 export default Button;
+*/
