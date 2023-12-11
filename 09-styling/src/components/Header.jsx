a1f10982803a328d8308f88a0fe1ca6168cf54ca
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-11 11:30:44 +0100

    refactor Header component styling using Tailwind CSS

--- 09-styling/src/components/Header.jsx
+++ 09-styling/src/components/Header.jsx
@@ -1,57 +1,21 @@
-import { styled } from "styled-components";
+// import { styled } from "styled-components"; // not needed when using Tailwind CSS
 
 import logo from '../assets/logo.png';
 // import classes from './Header.module.css'; // instead of import './Header.css';
 
-const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
+// the commented-out styled-components code can be found at the end of this file.
 
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
+// in Tailwind we use the regular HTML tags, so <StyledHeader></StyledHeader> will be changed to <header></header>:
 
-  & h1 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: 'Pacifico', cursive;
-    margin: 0;
-  }
+// then we can start adding the utility classes from Tailwind CSS (to the header tag for example):
 
-  & p {
-    text-align: center;
-    font-style: italic;
-    font-size: 1rem;
-    color: 'var(--color-primary)';
-    margin: 0;
-  }
-
-  @media (min-width: 768px) {
-    & {
-      margin-bottom: 4rem;
-    }
-
-    & h1 {
-      font-size: 2.25rem;
-    }
-`;
+// flex-col is short for flex-direction: column and is on "vertical axis" by default
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44"/>
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">ReactArt</h1>
       <p
         // this is how inline styles are added in React:
         // style={{
@@ -60,6 +24,52 @@ export default function Header() {
         //   color: 'var(--color-primary)',
         // }}
       >A community of artists and art-lovers.</p>
-    </StyledHeader>
+    </header>
   );
 }
+
+// <StyledHeader></StyledHeader> will again be changed to <header></header> when using Tailwind CSS.
+
+  // const StyledHeader = styled.header`
+  //   display: flex;
+  //   flex-direction: column;
+  //   align-items: center;
+  //   justify-content: center;
+  //   margin-top: 2rem;
+  //   margin-bottom: 2rem;
+
+  //   & img {
+  //     object-fit: contain;
+  //     margin-bottom: 2rem;
+  //     width: 11rem;
+  //     height: 11rem;
+  //   }
+
+  //   & h1 {
+  //     font-size: 1.5rem;
+  //     font-weight: 600;
+  //     letter-spacing: 0.4em;
+  //     text-align: center;
+  //     text-transform: uppercase;
+  //     color: #9a3412;
+  //     font-family: 'Pacifico', cursive;
+  //     margin: 0;
+  //   }
+
+  //   & p {
+  //     text-align: center;
+  //     font-style: italic;
+  //     font-size: 1rem;
+  //     color: 'var(--color-primary)';
+  //     margin: 0;
+  //   }
+
+  //   @media (min-width: 768px) {
+  //     & {
+  //       margin-bottom: 4rem;
+  //     }
+
+  //     & h1 {
+  //       font-size: 2.25rem;
+  //     }
+  // `;
