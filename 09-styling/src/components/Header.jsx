22482efc0c1cb1d840a465331dae334b43728c47
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-11 13:43:32 +0100

    update styling in Header component and add new font to Tailwind config

--- 09-styling/src/components/Header.jsx
+++ 09-styling/src/components/Header.jsx
@@ -15,8 +15,8 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
       <img src={logo} alt="A canvas" className="mb-8 w-44 h-44"/>
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">ReactArt</h1>
-      <p
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <p className="text-stone-500"
         // this is how inline styles are added in React:
         // style={{
         //   fontStyle: 'italic',
--- 09-styling/tailwind.config.js
+++ 09-styling/tailwind.config.js
@@ -6,7 +6,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"Pacifico"', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
+
+
+/*
+
+the "title" is a new name for a new font that is added to Tailwind. And the value for that is an array where we now need to specify the name of our font between double quotes inside of single quotes. (this is how it's required by Tailwind to work.)
+
+--> then, "font-title" among the classes where it gets applied
+
+*/
