6be5fb3dc5a71e857b8c50e2a7869bb8bb4a7a5b
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-13 14:48:08 +0100

    refactor Player component

--- 11-the-almost-final-countdown/src/components/Player.jsx
+++ 11-the-almost-final-countdown/src/components/Player.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 export default function Player() {
+  let name = "Player";
+  const [playerName, setPlayerName] = useState();
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name}</h2>
       <p>
-        <input type="text" />
+        <input
+          type="text"
+          defaultValue={name}
+        />
         <button>Set Name</button>
       </p>
     </section>
