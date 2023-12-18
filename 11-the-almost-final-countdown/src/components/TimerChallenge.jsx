35f0a1e1d8ae5750422db3b6cf52373ff45cb669
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-18 14:33:51 +0100

    add timer stop functionality and a ref hook

--- 11-the-almost-final-countdown/src/components/TimerChallenge.jsx
+++ 11-the-almost-final-countdown/src/components/TimerChallenge.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
 
-  const [timerStarted, setTimerStarted] = useState(false);
+  const timer = useRef(); // "timer" constant will be used to store the timer ID
 
-  // the state to know once the time is expired:
+  // states to start and stop the timer:
+  const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     // JS built-in function: a timer of 1000 milliseconds, that will execute the anonymous function after the timer has run out
-    setTimeout(() => {
+    timer.current = setTimeout(() => { // bcs you always must target the current value of the ref
       setTimerExpired(true);
     }, targetTime * 1000); // the time (that is in milliseconds) should be based on the targetTime prop, so we multiply the targetTime by 1000
 
     setTimerStarted(true);
+  }
+
+  function handleStop() {
+  // JS built-in function: clearTimerout() --> to stop the timer but it needs a pointer to the timer (its ID) that we want to stop, that's why we need to store the timer in a variable
+    clearTimeout(timer.current);
   }
 
   return <section className="challenge">
@@ -23,7 +29,7 @@ export default function TimerChallenge({ title, targetTime }) {
       {targetTime} second{targetTime > 1 ? "s" : ""}
     </p>
     <p>
-      <button onClick={handleStart}>{timerStarted ? "Stop" : "Start"} Challenge</button>
+      <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? "Stop" : "Start"} Challenge</button>
     </p>
     <p className={timerStarted ? "active" : undefined}>
       {timerStarted ? "Time is running..." : "Timer inactive"}
