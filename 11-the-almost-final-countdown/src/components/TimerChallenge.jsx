8a90631aec1644a05f24048b2cdc4c3dba5d8036
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-18 14:14:40 +0100

    refactor TimerChallenge component to improve timer functionality

--- 11-the-almost-final-countdown/src/components/TimerChallenge.jsx
+++ 11-the-almost-final-countdown/src/components/TimerChallenge.jsx
@@ -2,27 +2,31 @@ import { useState } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
 
+  const [timerStarted, setTimerStarted] = useState(false);
+
   // the state to know once the time is expired:
-  const [timeExpired, setTimeExpired] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     // JS built-in function: a timer of 1000 milliseconds, that will execute the anonymous function after the timer has run out
     setTimeout(() => {
-      setTimeExpired(true);
+      setTimerExpired(true);
     }, targetTime * 1000); // the time (that is in milliseconds) should be based on the targetTime prop, so we multiply the targetTime by 1000
+
+    setTimerStarted(true);
   }
 
   return <section className="challenge">
     <h2>{title}</h2>
-    {timeExpired && <p>Time is up! You lost!</p>}
+    {timerExpired && <p>Time is up! You lost!</p>}
     <p className="challenge-time">
       {targetTime} second{targetTime > 1 ? "s" : ""}
     </p>
     <p>
-      <button onClick={handleStart}>Start Challenge</button>
+      <button onClick={handleStart}>{timerStarted ? "Stop" : "Start"} Challenge</button>
     </p>
-    <p className="">
-      Time is running... / Timer inactive
+    <p className={timerStarted ? "active" : undefined}>
+      {timerStarted ? "Time is running..." : "Timer inactive"}
     </p>
   </section>
 }
