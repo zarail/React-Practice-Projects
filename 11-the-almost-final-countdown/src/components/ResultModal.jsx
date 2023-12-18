1d2af75aa1ff2696571fd1a73a23258e3d21b688
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-18 21:00:34 +0100

    add close button to ResultModal component

--- 11-the-almost-final-countdown/src/components/ResultModal.jsx
+++ 11-the-almost-final-countdown/src/components/ResultModal.jsx
@@ -3,6 +3,8 @@ export default function ResultModal(result, targetTime) {
     <h2>You {result}!</h2>
     <p>The target time was <strong>{targetTime}</strong> seconds.</p>
     <p>You stopped the timer with <strong>X seconds left</strong>.</p>
-    <form method="dialog"></form>
+    <form method="dialog">
+      <button>Close</button> {/* this will close the dialog */}
+    </form>
   </dialog>
 }
