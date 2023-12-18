0240ced4c5ed72196a9209f7e0fe9282bcf38650
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-18 21:14:15 +0100

    refactor ResultModal component

--- 11-the-almost-final-countdown/src/components/ResultModal.jsx
+++ 11-the-almost-final-countdown/src/components/ResultModal.jsx
@@ -1,10 +1,12 @@
-export default function ResultModal(result, targetTime) {
-  return <dialog className="result-modal" open> {/* has built-in styling & features ... set "open" to make it visible */}
-    <h2>You {result}!</h2>
-    <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-    <p>You stopped the timer with <strong>X seconds left</strong>.</p>
-    <form method="dialog">
-      <button>Close</button> {/* this will close the dialog */}
-    </form>
-  </dialog>
+export default function ResultModal({ result, targetTime }) {
+  return (
+    <dialog className="result-modal" open> {/* has built-in styling & features ... set "open" to make it visible */}
+      <h2>You {result}!</h2>
+      <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+      <p>You stopped the timer with <strong>X seconds left</strong>.</p>
+      <form method="dialog">
+        <button>Close</button> {/* this will close the dialog */}
+      </form>
+    </dialog>
+  )
 }
