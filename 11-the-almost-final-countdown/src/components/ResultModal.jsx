ff0d561e6fa3a9c5c6d51ad2a91eb836c4d34847
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-18 21:01:23 +0100

    update ResultModal styling

--- 11-the-almost-final-countdown/src/components/ResultModal.jsx
+++ 11-the-almost-final-countdown/src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 export default function ResultModal(result, targetTime) {
-  return <dialog open> {/* has built-in styling & features */}
+  return <dialog className="result-modal"> {/* has built-in styling & features */}
     <h2>You {result}!</h2>
     <p>The target time was <strong>{targetTime}</strong> seconds.</p>
     <p>You stopped the timer with <strong>X seconds left</strong>.</p>
