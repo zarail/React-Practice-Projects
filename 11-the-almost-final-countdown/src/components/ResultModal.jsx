406c0b7c37989e7b5e703615e90b61368d3847db
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-18 20:58:34 +0100

    add ResultModal component

---
+++ 11-the-almost-final-countdown/src/components/ResultModal.jsx
@@ -0,0 +1,8 @@
+export default function ResultModal(result, targetTime) {
+  return <dialog open> {/* has built-in styling & features */}
+    <h2>You {result}!</h2>
+    <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+    <p>You stopped the timer with <strong>X seconds left</strong>.</p>
+    <form method="dialog"></form>
+  </dialog>
+}
