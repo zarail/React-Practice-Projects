67d639e46339b631d555f5c321df33cf660da19c
Zahra Ilkhan <zahraa1991@gmail.com>
2023-12-19 18:13:52 +0100

    add score display in ResultModal component

--- 11-the-almost-final-countdown/src/components/ResultModal.jsx
+++ 11-the-almost-final-countdown/src/components/ResultModal.jsx
@@ -9,6 +9,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   // useImperativeHandle() is a hook that allows us to define which functions and values we want to expose to the parent component:
 
@@ -23,6 +24,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
   return (
     <dialog ref={dialog} className="result-modal"> {/* has built-in styling & features ... set "open" to make it visible */}
       {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>The target time was <strong>{targetTime} seconds</strong>.</p>
       <p>You stopped the timer with {' '} <strong>{formattedRemainingTime} seconds left</strong>.</p>
       <form method="dialog" onSubmit={onReset}>
