2eb86ff91ab67c858495f309b8f2743023c79117
Zahra Ilkhan <zahraa1991@gmail.com>
2024-01-08 17:11:20 +0100

    add ProjectsSidebar component and an "aside" element

---
+++ 12-project-management/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,9 @@
+export default function ProjectsSidebar() {
+  return <aside>
+    <h2 className="text-2xl font-bold">Projects</h2>
+  </aside>
+};
+
+/*
+the built-in "aside" element is used to represent a section of a page that consists of content that is tangentially related to the content around the aside element, and which could be considered separate from that content. Such sections are often represented as sidebars in printed typography.
+*/
