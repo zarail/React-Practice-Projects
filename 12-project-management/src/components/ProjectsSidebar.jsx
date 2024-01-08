11e0b1b03aab2a62dcce20fef2c11e8618c0ab0c
Zahra Ilkhan <zahraa1991@gmail.com>
2024-01-08 17:34:11 +0100

    update ProjectsSidebar and App components

--- 12-project-management/src/components/ProjectsSidebar.jsx
+++ 12-project-management/src/components/ProjectsSidebar.jsx
@@ -1,6 +1,11 @@
 export default function ProjectsSidebar() {
-  return <aside>
-    <h2 className="text-2xl font-bold">Projects</h2>
+  return <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+    <h2 className="text-2xl font-bold">My Projects</h2>
+    <div>
+      <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+        Add Project
+      </button>
+    </div>
   </aside>
 };
 
